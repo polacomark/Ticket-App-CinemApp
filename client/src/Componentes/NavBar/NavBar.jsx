ff3401b99fd84ff0b3acdd468c31ffb91bcc686d
DanielMart-tech <danieljmp07@gmail.com>
2022-04-29 16:15:07 -0400

    cambios menores

--- client/src/Componentes/NavBar/NavBar.jsx
+++ client/src/Componentes/NavBar/NavBar.jsx
@@ -1,23 +1,32 @@
-import * as React from 'react';
-import MuiAppBar from '@material-ui/core/AppBar';
-import MenuIcon from '@material-ui/icons/Menu';
+import * as React from "react";
+import MuiAppBar from "@material-ui/core/AppBar";
+import MenuIcon from "@material-ui/icons/Menu";
 import { styled, alpha, makeStyles } from "@material-ui/core/styles";
-import { AccountCircle, More, Search } from '@material-ui/icons';
-import { InputBase, Menu, MenuItem, IconButton, Typography, Toolbar } from '@material-ui/core';
-import { logout, moviesByName } from '../../redux/actions';
-import { useDispatch } from 'react-redux';
-import { Link, useNavigate } from "react-router-dom";
+import { AccountCircle, More, Search } from "@material-ui/icons";
+import {
+  InputBase,
+  Menu,
+  MenuItem,
+  IconButton,
+  Typography,
+  Toolbar,
+} from "@material-ui/core";
+import { logout, moviesByName } from "../../redux/actions";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 const drawerWidth = 360;
 
-const AppBar = styled(MuiAppBar, {shouldForwardProp: (prop) => prop !== 'open'})(({ theme, open }) => ({
-  transition: theme.transitions.create(['margin', 'width'], {
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== "open",
+})(({ theme, open }) => ({
+  transition: theme.transitions.create(["margin", "width"], {
     easing: theme.transitions.easing.sharp,
     duration: theme.transitions.duration.leavingScreen,
   }),
   ...(open && {
     width: `calc(100% - ${drawerWidth}px)`,
-    transition: theme.transitions.create(['margin', 'width'], {
+    transition: theme.transitions.create(["margin", "width"], {
       easing: theme.transitions.easing.easeOut,
       duration: theme.transitions.duration.enteringScreen,
     }),
@@ -26,76 +35,75 @@ const AppBar = styled(MuiAppBar, {shouldForwardProp: (prop) => prop !== 'open'})
 }));
 
 const useStyles = makeStyles((theme) => ({
-    btnFiltros:{
-      marginRight: theme.spacing(2),
-      [theme.breakpoints.up('sm')]:{
-        display: 'flex',
-      }
+  btnFiltros: {
+    marginRight: theme.spacing(2),
+    [theme.breakpoints.up("sm")]: {
+      display: "flex",
     },
-    root:{
-      backgroundColor: '#373E47',
-      color: '#fff',
+  },
+  root: {
+    backgroundColor: "#373E47",
+    color: "#fff",
+  },
+  grow: {
+    flexGrow: 1,
+  },
+  title: {
+    display: "none",
+    [theme.breakpoints.up("sm")]: {
+      display: "block",
     },
-    grow: {
-      flexGrow: 1,
+  },
+  search: {
+    position: "relative",
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: alpha(theme.palette.common.white, 0.15),
+    "&:hover": {
+      backgroundColor: alpha(theme.palette.common.white, 0.25),
     },
-    title: {
-      display: 'none',
-      [theme.breakpoints.up('sm')]: {
-        display: 'block',
-      },
+    marginRight: theme.spacing(2),
+    marginLeft: 0,
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      marginLeft: theme.spacing(3),
+      width: "auto",
     },
-    search: {
-      position: 'relative',
-      borderRadius: theme.shape.borderRadius,
-      backgroundColor: alpha(theme.palette.common.white, 0.15),
-      '&:hover': {
-        backgroundColor: alpha(theme.palette.common.white, 0.25),
-      },
-      marginRight: theme.spacing(2),
-      marginLeft: 0,
-      width: '100%',
-      [theme.breakpoints.up('sm')]: {
-        marginLeft: theme.spacing(3),
-        width: 'auto',
-      },
+  },
+  searchIcon: {
+    padding: theme.spacing(0, 2),
+    height: "100%",
+    position: "absolute",
+    pointerEvents: "none",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  inputRoot: {
+    color: "inherit",
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(2)}px)`,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("md")]: {
+      width: "20ch",
     },
-    searchIcon: {
-      padding: theme.spacing(0, 2),
-      height: '100%',
-      position: 'absolute',
-      pointerEvents: 'none',
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center',
+  },
+  sectionDesktop: {
+    display: "none",
+    [theme.breakpoints.up("md")]: {
+      display: "flex",
     },
-    inputRoot: {
-      color: 'inherit',
+  },
+  sectionMobile: {
+    display: "flex",
+    [theme.breakpoints.up("md")]: {
+      display: "none",
     },
-    inputInput: {
-      padding: theme.spacing(1, 1, 1, 0),
-      // vertical padding + font size from searchIcon
-      paddingLeft: `calc(1em + ${theme.spacing(2)}px)`,
-      transition: theme.transitions.create('width'),
-      width: '100%',
-      [theme.breakpoints.up('md')]: {
-        width: '20ch',
-      },
-    },
-    sectionDesktop: {
-      display: 'none',
-      [theme.breakpoints.up('md')]: {
-        display: 'flex',
-      },
-    },
-    sectionMobile: {
-      display: 'flex',
-      [theme.breakpoints.up('md')]: {
-        display: 'none',
-      },
-    },
-    
-  }));
+  },
+}));
 
 function NavBar(props) {
   const dispatch = useDispatch();
@@ -103,7 +111,7 @@ function NavBar(props) {
   const [anchorEl, setAnchorEl] = React.useState(null);
   const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
 
-  const [search, setSearch] = useState('')
+  const [search, setSearch] = React.useState("");
   const navigate = useNavigate();
 
   const isMenuOpen = Boolean(anchorEl);
@@ -111,7 +119,6 @@ function NavBar(props) {
 
   const handleProfileMenuOpen = (event) => {
     setAnchorEl(event.currentTarget);
-   
   };
 
   const handleMobileMenuClose = () => {
@@ -121,7 +128,7 @@ function NavBar(props) {
   const handleMenuClose = () => {
     setAnchorEl(null);
     handleMobileMenuClose();
-    navigate('/perfil')
+    navigate("/perfil");
   };
 
   const handleMobileMenuOpen = (event) => {
@@ -132,25 +139,26 @@ function NavBar(props) {
     e.preventDefault();
     setSearch(e.target.value);
     // console.log(search)
-  }
+  };
 
   const handleSubmitSearch = (e) => {
     e.preventDefault();
     dispatch(moviesByName(search));
-  }
+  };
 
   const handleLogOut = (e) => {
     e.preventDefault();
     dispatch(logout());
-  }
+  };
 
-  const menuId = 'primary-search-account-menu';
+  const menuId = "primary-search-account-menu";
   const renderMenu = (
-    <Menu anchorEl={anchorEl}
-      anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+    <Menu
+      anchorEl={anchorEl}
+      anchorOrigin={{ vertical: "top", horizontal: "right" }}
       id={menuId}
       keepMounted
-      transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+      transformOrigin={{ vertical: "top", horizontal: "right" }}
       open={isMenuOpen}
       onClose={handleMenuClose}
     >
@@ -159,14 +167,14 @@ function NavBar(props) {
     </Menu>
   );
 
-  const mobileMenuId = 'primary-search-account-menu-mobile';
+  const mobileMenuId = "primary-search-account-menu-mobile";
   const renderMobileMenu = (
     <Menu
       anchorEl={mobileMoreAnchorEl}
-      anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+      anchorOrigin={{ vertical: "top", horizontal: "right" }}
       id={mobileMenuId}
       keepMounted
-      transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+      transformOrigin={{ vertical: "top", horizontal: "right" }}
       open={isMobileMenuOpen}
       onClose={handleMobileMenuClose}
     >
@@ -185,30 +193,34 @@ function NavBar(props) {
   );
   return (
     <div className={classes.grow}>
-      <AppBar position='fixed' open={props.open} style={{backgroundColor:'#373E47', color:'white'}} >
-        <Toolbar >
-          <IconButton className={classes.btnFiltros} 
-            color="inherit" 
+      <AppBar
+        position="fixed"
+        open={props.open}
+        style={{ backgroundColor: "#373E47", color: "white" }}
+      >
+        <Toolbar>
+          <IconButton
+            className={classes.btnFiltros}
+            color="inherit"
             aria-label="open drawer"
             edge="start"
             onClick={props.handleDrawerOpen}
-            sx={{ ...(props.open && { display: 'none' }) }}
+            sx={{ ...(props.open && { display: "none" }) }}
           >
             <MenuIcon />
           </IconButton>
           <Typography className={classes.title} variant="h6" noWrap>
-            Cinem<span style={{'color':'#5ED5A8'}}>App</span>
+            Cinem<span style={{ color: "#5ED5A8" }}>App</span>
           </Typography>
           <div className={classes.search}>
-            <div className={classes.searchIcon}>
-            </div>
+            <div className={classes.searchIcon}></div>
             <InputBase
               placeholder="Buscar Película…"
               classes={{
                 root: classes.inputRoot,
                 input: classes.inputInput,
               }}
-              inputProps={{ 'aria-label': 'search' }}
+              inputProps={{ "aria-label": "search" }}
               onChange={handleChangeSearch}
             />
           </div>
@@ -217,7 +229,8 @@ function NavBar(props) {
           </div>
           <div className={classes.grow} />
           <div className={classes.sectionDesktop}>
-            <IconButton edge="end"
+            <IconButton
+              edge="end"
               aria-label="account of current user"
               aria-controls={menuId}
               aria-haspopup="true"
@@ -228,13 +241,14 @@ function NavBar(props) {
             </IconButton>
           </div>
           <div className={classes.sectionMobile}>
-            <IconButton aria-label="show more"
+            <IconButton
+              aria-label="show more"
               aria-controls={mobileMenuId}
               aria-haspopup="true"
               onClick={handleMobileMenuOpen}
               color="primary"
             >
-              <More/>
+              <More />
             </IconButton>
           </div>
         </Toolbar>
@@ -245,4 +259,4 @@ function NavBar(props) {
   );
 }
 
-export default NavBar
+export default NavBar;
